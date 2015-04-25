efae91b0f17b23723ff76290a2f538cf102d7e56
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-04-25 16:41:44 +0200

    Update #6

--- LaurenceActivity/WebFolder/index.waPage/scripts/index.js
+++ LaurenceActivity/WebFolder/index.waPage/scripts/index.js
@@ -2,11 +2,18 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var menuItem8 = {};	// @menuItem
 	var documentEvent = {};	// @document
 	var W_Login = {};	// @login
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	menuItem8.click = function menuItem8_click (event)// @startlock
+	{// @endlock
+		$$("cchg").show();
+		$$('component1').loadComponent("/Calendrier.waComponent");
+	};// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
@@ -16,12 +23,22 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		 } else {
 		 	
 		 	if (waf.directory.currentUserBelongsTo("Elève")) {
-				//$$('M_Ressources').hide();
-				//$$('M_ActivE').hide();
+				$$('M_Invite').hide();
+				$$('M_Professeur').hide();
+				$$('M_Admin').hide();
 			}
 			if (waf.directory.currentUserBelongsTo("Professeur")) {
 
-				//$$('M_ActivP').hide();
+				$$('M_Invite').hide();
+				$$('M_Eleve').hide();
+				$$('M_Admin').hide();
+				
+			}
+			if (waf.directory.currentUserBelongsTo("Invité")) {
+
+				$$('M_Professeur').hide();
+				$$('M_Eleve').hide();
+				$$('M_Admin').hide();
 				
 			}
 			
@@ -48,6 +65,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("menuItem8", "click", menuItem8.click, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 	WAF.addListener("W_Login", "logout", W_Login.logout, "WAF");
 	WAF.addListener("W_Login", "login", W_Login.login, "WAF");
