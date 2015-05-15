c38468650f4391ad4bf49fc4e9429ced6b287fbf
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-05-15 20:06:44 +0200

    Update #43

--- LaurenceActivity/WebFolder/Gest_Cal.waComponent/Gest_Cal.js
+++ LaurenceActivity/WebFolder/Gest_Cal.waComponent/Gest_Cal.js
@@ -21,6 +21,8 @@ function constructor (id) {
 		for (var i = 0; i < 36; i++) {
 			v = "component1_j"+i;
 			$$(v).hide();
+			v = "component1_i"+i;
+			$$(v).hide();
 			v = "component1_m"+i;
 			$$(v).show();
 			$$(v).setBackgroundColor("#E5E5E5");
@@ -157,6 +159,12 @@ function constructor (id) {
 										$$(vBox).setValue(vAssoc);
 										$$(vBox).setBackgroundColor(vCoul);
 									}
+									if (elem.Message.length > 0) {
+										vBox = "component1_t"+iBox;
+										$$(vBox).setValue(elem.Message);
+										vBox = "component1_i"+iBox;
+										$$(vBox).show();
+									}
 								}});
 							}
 							}, params:[dbcl, vAnScol], userData: {boxn:ibx}});
@@ -180,6 +188,12 @@ function constructor (id) {
 										vBox = "component1_am"+iBox;
 										$$(vBox).setValue(vAssoc);
 										$$(vBox).setBackgroundColor(vCoul);
+									}
+									if (elem.Description.length > 0) {
+										vBox = "component1_t"+iBox;
+										$$(vBox).setValue(elem.Description);
+										vBox = "component1_i"+iBox;
+										$$(vBox).show();
 									}
 								}});
 							}
@@ -259,12 +273,444 @@ function constructor (id) {
 	}
 
 	// @region namespaceDeclaration// @startlock
+	var i1 = {};	// @icon
+	var i2 = {};	// @icon
+	var i3 = {};	// @icon
+	var i4 = {};	// @icon
+	var i5 = {};	// @icon
+	var i6 = {};	// @icon
+	var i7 = {};	// @icon
+	var i8 = {};	// @icon
+	var i9 = {};	// @icon
+	var i10 = {};	// @icon
+	var i11 = {};	// @icon
+	var i12 = {};	// @icon
+	var i13 = {};	// @icon
+	var i14 = {};	// @icon
+	var i15 = {};	// @icon
+	var i16 = {};	// @icon
+	var i17 = {};	// @icon
+	var i18 = {};	// @icon
+	var i19 = {};	// @icon
+	var i20 = {};	// @icon
+	var i21 = {};	// @icon
+	var i22 = {};	// @icon
+	var i23 = {};	// @icon
+	var i24 = {};	// @icon
+	var i25 = {};	// @icon
+	var i26 = {};	// @icon
+	var i27 = {};	// @icon
+	var i28 = {};	// @icon
+	var i29 = {};	// @icon
+	var i30 = {};	// @icon
+	var i31 = {};	// @icon
+	var i32 = {};	// @icon
+	var i33 = {};	// @icon
+	var i34 = {};	// @icon
+	var i35 = {};	// @icon
+	var i0 = {};	// @icon
 	var cbxTout = {};	// @checkbox
 	var cbAnScol = {};	// @combobox
 	var sPerS = {};	// @slider
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
+
+	i1.mouseover = function i1_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t1").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i1.mouseout = function i1_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i2.mouseover = function i2_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t2").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i2.mouseout = function i2_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i3.mouseover = function i3_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t3").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i3.mouseout = function i3_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i4.mouseover = function i4_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t4").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i4.mouseout = function i4_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i5.mouseover = function i5_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t5").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i5.mouseout = function i5_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i6.mouseover = function i6_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t6").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i6.mouseout = function i6_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i7.mouseover = function i7_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t7").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i7.mouseout = function i7_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i8.mouseover = function i8_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t8").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i8.mouseout = function i8_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i9.mouseover = function i9_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t9").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i9.mouseout = function i9_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i10.mouseover = function i10_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t10").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i10.mouseout = function i10_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i11.mouseover = function i11_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t11").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i11.mouseout = function i11_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i12.mouseover = function i12_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t12").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i12.mouseout = function i12_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i13.mouseover = function i13_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t13").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i13.mouseout = function i13_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i14.mouseover = function i14_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t14").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i14.mouseout = function i14_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i15.mouseover = function i15_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t15").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i15.mouseout = function i15_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i16.mouseover = function i16_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t16").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i16.mouseout = function i16_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i17.mouseover = function i17_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t17").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i17.mouseout = function i17_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i18.mouseover = function i18_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t18").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i18.mouseout = function i18_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i19.mouseover = function i19_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t19").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i19.mouseout = function i19_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i20.mouseover = function i20_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t20").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i20.mouseout = function i20_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i21.mouseover = function i21_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t21").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i21.mouseout = function i21_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i22.mouseover = function i22_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t22").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i22.mouseout = function i22_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i23.mouseover = function i23_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t23").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i23.mouseout = function i23_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i24.mouseover = function i24_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t24").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i24.mouseout = function i24_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i25.mouseover = function i25_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t25").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i25.mouseout = function i25_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i26.mouseover = function i26_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t26").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i26.mouseout = function i26_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i27.mouseover = function i27_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t27").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i27.mouseout = function i27_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i28.mouseover = function i28_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t28").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i28.mouseout = function i28_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i29.mouseover = function i29_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t29").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i29.mouseout = function i29_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i30.mouseover = function i30_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t30").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i30.mouseout = function i30_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i31.mouseover = function i31_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t31").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i31.mouseout = function i31_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i32.mouseover = function i32_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t32").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i32.mouseout = function i32_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i33.mouseover = function i33_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t33").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i33.mouseout = function i33_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i34.mouseover = function i34_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t34").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i34.mouseout = function i34_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i35.mouseover = function i35_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t35").getValue());
+		$$("component1_tMess").show();
+	};// @lock
+
+	i35.mouseout = function i35_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i0.mouseout = function i0_mouseout (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").hide();
+	};// @lock
+
+	i0.mouseover = function i0_mouseover (event)// @startlock
+	{// @endlock
+		$$("component1_tMess").setValue($$("component1_t0").getValue());
+		$$("component1_tMess").show();
+	};// @lock
 
 	cbxTout.click = function cbxTout_click (event)// @startlock
 	{// @endlock
@@ -297,6 +743,78 @@ function constructor (id) {
 	};// @lock
 
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_i1", "mouseover", i1.mouseover, "WAF");
+	WAF.addListener(this.id + "_i1", "mouseout", i1.mouseout, "WAF");
+	WAF.addListener(this.id + "_i2", "mouseover", i2.mouseover, "WAF");
+	WAF.addListener(this.id + "_i2", "mouseout", i2.mouseout, "WAF");
+	WAF.addListener(this.id + "_i3", "mouseover", i3.mouseover, "WAF");
+	WAF.addListener(this.id + "_i3", "mouseout", i3.mouseout, "WAF");
+	WAF.addListener(this.id + "_i4", "mouseover", i4.mouseover, "WAF");
+	WAF.addListener(this.id + "_i4", "mouseout", i4.mouseout, "WAF");
+	WAF.addListener(this.id + "_i5", "mouseover", i5.mouseover, "WAF");
+	WAF.addListener(this.id + "_i5", "mouseout", i5.mouseout, "WAF");
+	WAF.addListener(this.id + "_i6", "mouseover", i6.mouseover, "WAF");
+	WAF.addListener(this.id + "_i6", "mouseout", i6.mouseout, "WAF");
+	WAF.addListener(this.id + "_i7", "mouseover", i7.mouseover, "WAF");
+	WAF.addListener(this.id + "_i7", "mouseout", i7.mouseout, "WAF");
+	WAF.addListener(this.id + "_i8", "mouseover", i8.mouseover, "WAF");
+	WAF.addListener(this.id + "_i8", "mouseout", i8.mouseout, "WAF");
+	WAF.addListener(this.id + "_i9", "mouseover", i9.mouseover, "WAF");
+	WAF.addListener(this.id + "_i9", "mouseout", i9.mouseout, "WAF");
+	WAF.addListener(this.id + "_i10", "mouseover", i10.mouseover, "WAF");
+	WAF.addListener(this.id + "_i10", "mouseout", i10.mouseout, "WAF");
+	WAF.addListener(this.id + "_i11", "mouseover", i11.mouseover, "WAF");
+	WAF.addListener(this.id + "_i11", "mouseout", i11.mouseout, "WAF");
+	WAF.addListener(this.id + "_i12", "mouseover", i12.mouseover, "WAF");
+	WAF.addListener(this.id + "_i12", "mouseout", i12.mouseout, "WAF");
+	WAF.addListener(this.id + "_i13", "mouseover", i13.mouseover, "WAF");
+	WAF.addListener(this.id + "_i13", "mouseout", i13.mouseout, "WAF");
+	WAF.addListener(this.id + "_i14", "mouseover", i14.mouseover, "WAF");
+	WAF.addListener(this.id + "_i14", "mouseout", i14.mouseout, "WAF");
+	WAF.addListener(this.id + "_i15", "mouseover", i15.mouseover, "WAF");
+	WAF.addListener(this.id + "_i15", "mouseout", i15.mouseout, "WAF");
+	WAF.addListener(this.id + "_i16", "mouseover", i16.mouseover, "WAF");
+	WAF.addListener(this.id + "_i16", "mouseout", i16.mouseout, "WAF");
+	WAF.addListener(this.id + "_i17", "mouseover", i17.mouseover, "WAF");
+	WAF.addListener(this.id + "_i17", "mouseout", i17.mouseout, "WAF");
+	WAF.addListener(this.id + "_i18", "mouseover", i18.mouseover, "WAF");
+	WAF.addListener(this.id + "_i18", "mouseout", i18.mouseout, "WAF");
+	WAF.addListener(this.id + "_i19", "mouseover", i19.mouseover, "WAF");
+	WAF.addListener(this.id + "_i19", "mouseout", i19.mouseout, "WAF");
+	WAF.addListener(this.id + "_i20", "mouseover", i20.mouseover, "WAF");
+	WAF.addListener(this.id + "_i20", "mouseout", i20.mouseout, "WAF");
+	WAF.addListener(this.id + "_i21", "mouseover", i21.mouseover, "WAF");
+	WAF.addListener(this.id + "_i21", "mouseout", i21.mouseout, "WAF");
+	WAF.addListener(this.id + "_i22", "mouseover", i22.mouseover, "WAF");
+	WAF.addListener(this.id + "_i22", "mouseout", i22.mouseout, "WAF");
+	WAF.addListener(this.id + "_i23", "mouseover", i23.mouseover, "WAF");
+	WAF.addListener(this.id + "_i23", "mouseout", i23.mouseout, "WAF");
+	WAF.addListener(this.id + "_i24", "mouseover", i24.mouseover, "WAF");
+	WAF.addListener(this.id + "_i24", "mouseout", i24.mouseout, "WAF");
+	WAF.addListener(this.id + "_i25", "mouseover", i25.mouseover, "WAF");
+	WAF.addListener(this.id + "_i25", "mouseout", i25.mouseout, "WAF");
+	WAF.addListener(this.id + "_i26", "mouseover", i26.mouseover, "WAF");
+	WAF.addListener(this.id + "_i26", "mouseout", i26.mouseout, "WAF");
+	WAF.addListener(this.id + "_i27", "mouseover", i27.mouseover, "WAF");
+	WAF.addListener(this.id + "_i27", "mouseout", i27.mouseout, "WAF");
+	WAF.addListener(this.id + "_i28", "mouseover", i28.mouseover, "WAF");
+	WAF.addListener(this.id + "_i28", "mouseout", i28.mouseout, "WAF");
+	WAF.addListener(this.id + "_i29", "mouseover", i29.mouseover, "WAF");
+	WAF.addListener(this.id + "_i29", "mouseout", i29.mouseout, "WAF");
+	WAF.addListener(this.id + "_i30", "mouseover", i30.mouseover, "WAF");
+	WAF.addListener(this.id + "_i30", "mouseout", i30.mouseout, "WAF");
+	WAF.addListener(this.id + "_i31", "mouseover", i31.mouseover, "WAF");
+	WAF.addListener(this.id + "_i31", "mouseout", i31.mouseout, "WAF");
+	WAF.addListener(this.id + "_i32", "mouseover", i32.mouseover, "WAF");
+	WAF.addListener(this.id + "_i32", "mouseout", i32.mouseout, "WAF");
+	WAF.addListener(this.id + "_i33", "mouseover", i33.mouseover, "WAF");
+	WAF.addListener(this.id + "_i33", "mouseout", i33.mouseout, "WAF");
+	WAF.addListener(this.id + "_i34", "mouseover", i34.mouseover, "WAF");
+	WAF.addListener(this.id + "_i34", "mouseout", i34.mouseout, "WAF");
+	WAF.addListener(this.id + "_i35", "mouseover", i35.mouseover, "WAF");
+	WAF.addListener(this.id + "_i35", "mouseout", i35.mouseout, "WAF");
+	WAF.addListener(this.id + "_i0", "mouseout", i0.mouseout, "WAF");
+	WAF.addListener(this.id + "_i0", "mouseover", i0.mouseover, "WAF");
 	WAF.addListener(this.id + "_cbxTout", "click", cbxTout.click, "WAF");
 	WAF.addListener(this.id + "_cbAnScol", "change", cbAnScol.change, "WAF");
 	WAF.addListener(this.id + "_sPerS", "slidestop", sPerS.slidestop, "WAF");
