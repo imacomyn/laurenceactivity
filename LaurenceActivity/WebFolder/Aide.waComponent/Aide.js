13dcb6dd856e87799675971c56fcf90cf6f6a441
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-08-28 17:06:12 +0200

    Update #62

--- LaurenceActivity/WebFolder/Aide.waComponent/Aide.js
+++ LaurenceActivity/WebFolder/Aide.waComponent/Aide.js
@@ -15,17 +15,24 @@ function constructor (id) {
 	$$("cchg").hide();
 
 	// @region namespaceDeclaration// @startlock
+	var btB = {};	// @buttonImage
 	var btPgm = {};	// @buttonImage
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
-	btPgm.click = function btPgm_click (event)// @startlock
+	btB.click = function btB_click (event)// @startlock
 	{// @endlock
 		$$('component1_video1').loadVideoByUrl("/Videos/SRM2015.mp4");
 	};// @lock
 
+	btPgm.click = function btPgm_click (event)// @startlock
+	{// @endlock
+		$$('component1_video1').loadVideoByUrl("/Videos/SRM2014.mp4");
+	};// @lock
+
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_btB", "click", btB.click, "WAF");
 	WAF.addListener(this.id + "_btPgm", "click", btPgm.click, "WAF");
 	// @endregion// @endlock
 
