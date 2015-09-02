3f7680335647b6bec367cb808437381a788b3845
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-09-02 10:36:00 +0200

    Update #68

--- LaurenceActivity/WebFolder/index.waPage/scripts/index-tablet.js
+++ LaurenceActivity/WebFolder/index.waPage/scripts/index-tablet.js
@@ -2,11 +2,27 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var btClose = {};	// @buttonImage
+	var btShow = {};	// @buttonImage
 	var cSel = {};	// @select
 	var W_Login = {};	// @login
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	btClose.click = function btClose_click (event)// @startlock
+	{// @endlock
+		$$('btShow').show();
+		$$('btClose').hide();
+		$$('sPDF').hide();
+	};// @lock
+
+	btShow.click = function btShow_click (event)// @startlock
+	{// @endlock
+		$$('btShow').hide();
+		$$('btClose').show();
+		$$('sPDF').show();
+	};// @lock
 
 	cSel.change = function cSel_change (event)// @startlock
 	{// @endlock
@@ -14,19 +30,25 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		
 		$$('LstPDF').hide();
 		$$('sPDF').hide();
+		$$('btShow').hide();
+		$$('btClose').hide();
+		$$('rPhoto').hide();
+		
 		vSel = $$('cSel').getValue();
 		
 		if (vSel === "Cours PDF") {
 			vUser = WAF.directory.currentUser().userName;
+			$$("LstPDF").setRowHeight(26);
 			sources.eleves.query("Utilisateur.Login = :1", { onSuccess: function(event) { 
-					alert("entre");
 					var vEleID;
 					elem = sources.eleves;
 					if (elem.length > 0) {
 						vEleID = elem.ID;
 						sources.abonnes.query("Eleve.ID = :1", vEleID);
 						$$('LstPDF').show();
-						$$('sPDF').show();
+						$$('btShow').show();
+						$$('rPhoto').show();
+						$$('sPDF').hide();
 					};		
 			}, params:[vUser] });
 		}
@@ -53,6 +75,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("btClose", "click", btClose.click, "WAF");
+	WAF.addListener("btShow", "click", btShow.click, "WAF");
 	WAF.addListener("cSel", "change", cSel.change, "WAF");
 	WAF.addListener("W_Login", "login", W_Login.login, "WAF");
 	WAF.addListener("W_Login", "logout", W_Login.logout, "WAF");
