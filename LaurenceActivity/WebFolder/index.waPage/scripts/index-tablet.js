8bd136338fca452c918cd9919aa53defc14256e0
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-09-02 12:59:22 +0200

    Update #69

--- LaurenceActivity/WebFolder/index.waPage/scripts/index-tablet.js
+++ LaurenceActivity/WebFolder/index.waPage/scripts/index-tablet.js
@@ -2,13 +2,186 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var ListAssoc = {};	// @dataGrid
+	var btRight = {};	// @image
+	var btLeft = {};	// @image
+	var btPlan = {};	// @buttonImage
+	var btNews = {};	// @buttonImage
+	var btFilm = {};	// @buttonImage
+	var btPDF = {};	// @buttonImage
+	var documentEvent = {};	// @document
+	var LstPDF = {};	// @dataGrid
 	var btClose = {};	// @buttonImage
 	var btShow = {};	// @buttonImage
-	var cSel = {};	// @select
 	var W_Login = {};	// @login
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	ListAssoc.onRowDraw = function ListAssoc_onRowDraw (event)// @startlock
+	{// @endlock
+		var vAssoc, vToday, vID;
+		elem = sources.Association1;
+		vID = elem.ID;
+		if (vID !== null) {
+			vAssoc = elem.Sigle;
+			vToday = new Date();
+				sources.informations.query("Annee_Scolaire.Association.Sigle = :1 and ( Fin_Valide = null or Fin_Valide > :2 ) order by Date_Creation desc", { onSuccess: function(event) { 
+					elem = sources.informations;
+					$$('cCount').setValue(elem.length);
+					$$('cPos').setValue(0);
+				}, params:[vAssoc, vToday] });
+		}
+	};// @lock
+
+	btRight.click = function btRight_click (event)// @startlock
+	{// @endlock
+		var vPos, vCount, vFrame;
+		
+		vPos = parseInt($$('cPos').getValue(),10);
+		vCount = parseInt($$('cCount').getValue(),10);
+		vPos += 1;
+		if (vPos >= vCount) {
+			vPos = 0;
+		}
+		$$('ListActu').setSelectedRows([vPos]);
+		$$('cPos').setValue(vPos);
+	};// @lock
+
+	btLeft.click = function btLeft_click (event)// @startlock
+	{// @endlock
+		var vPos, vCount, vFrame;
+		
+		vPos = parseInt($$('cPos').getValue(),10);
+		vCount = parseInt($$('cCount').getValue(),10);
+		vPos -= 1;
+		if (vPos < 0) {
+			vPos = vCount-1;
+		}
+		$$('ListActu').setSelectedRows([vPos]);
+		$$('cPos').setValue(vPos);
+	};// @lock
+
+	btPlan.click = function btPlan_click (event)// @startlock
+	{// @endlock
+		$$('LstPDF').hide();
+		$$('sPDF').hide();
+		$$('btShow').hide();
+		$$('btClose').hide();
+		$$('rPhoto').hide();
+		$$('cTitre').hide();
+		$$('cDesc').hide();
+		$$('cCreat').hide();
+		$$('cPhoto').hide();
+		$$('cComm').hide();
+		$$('cLogo').hide();
+		$$('cEntete').hide();
+		$$('btRight').hide();
+		$$('btLeft').hide();
+	};// @lock
+
+	btNews.click = function btNews_click (event)// @startlock
+	{// @endlock
+		$$('LstPDF').hide();
+		$$('sPDF').hide();
+		$$('btShow').hide();
+		$$('btClose').hide();
+		$$('rPhoto').hide();
+		$$('cTitre').show();
+		$$('cDesc').show();
+		$$('cCreat').show();
+		$$('cPhoto').show();
+		$$('cComm').show();
+		$$('cLogo').show();
+		$$('cEntete').show();
+		$$('btLeft').show();
+		$$('btRight').show();
+		
+	};// @lock
+
+	btFilm.click = function btFilm_click (event)// @startlock
+	{// @endlock
+		$$('LstPDF').hide();
+		$$('sPDF').hide();
+		$$('btShow').hide();
+		$$('btClose').hide();
+		$$('rPhoto').hide();
+		$$('cTitre').hide();
+		$$('cDesc').hide();
+		$$('cCreat').hide();
+		$$('cPhoto').hide();
+		$$('cComm').hide();
+		$$('cLogo').hide();
+		$$('cEntete').hide();
+		$$('btLeft').hide();
+		$$('btRight').hide();
+	};// @lock
+
+	btPDF.click = function btPDF_click (event)// @startlock
+	{// @endlock
+		var vSel, vUser, vRole, vQuery, vEleID;
+		
+		$$("LstPDF").setRowHeight(26);
+		$$('cTitre').hide();
+		$$('cDesc').hide();
+		$$('cCreat').hide();
+		$$('cPhoto').hide();
+		$$('cComm').hide();
+		$$('cLogo').hide();
+		$$('cEntete').hide();
+		$$('btRight').hide();
+		$$('btLeft').hide();
+			
+		elem = sources.eleves;
+		if (elem.length > 0) {
+			vEleID = elem.ID;
+			sources.abonnes.query("Eleve.ID = :1", vEleID);
+			$$('LstPDF').show();
+			$$('btShow').show();
+			$$('rPhoto').show();
+			$$('sPDF').hide();
+		};		
+
+	};// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		var vUser, vUserName;
+		
+		vUser = WAF.directory.currentUser();
+		
+		if (vUser === null) {
+			$$('main').destroy();
+		 } else {
+		 	
+		 	if (waf.directory.currentUserBelongsTo("Elève")) {
+								
+				vUserName = waf.directory.currentUser().userName;
+				sources.eleves.query("Utilisateur.Login = :1",vUserName);
+			}
+			if (waf.directory.currentUserBelongsTo("Professeur")) {
+
+				alert("Aucune fonction n'est disponible pour un professeur en version tablette. Vous devez utiliser ALT-C sur un ordinateur. Merci de votre compréhension");
+				$$('main').destroy();
+				
+			}
+			if (waf.directory.currentUserBelongsTo("Administrateur")) {
+
+				alert("Aucune fonction n'est disponible pour un administrateur en version tablette. Vous devez utiliser ALT-C sur un ordinateur. Merci de votre compréhension");
+				$$('main').destroy();
+				
+			}
+			
+		 }
+
+	};// @lock
+
+	LstPDF.onRowClick = function LstPDF_onRowClick (event)// @startlock
+	{// @endlock
+		$$('btShow').show();
+		$$('btClose').hide();
+		$$('sPDF').hide();
+	};// @lock
 
 	btClose.click = function btClose_click (event)// @startlock
 	{// @endlock
@@ -22,38 +195,6 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		$$('btShow').hide();
 		$$('btClose').show();
 		$$('sPDF').show();
-	};// @lock
-
-	cSel.change = function cSel_change (event)// @startlock
-	{// @endlock
-		var vSel, vUser;
-		
-		$$('LstPDF').hide();
-		$$('sPDF').hide();
-		$$('btShow').hide();
-		$$('btClose').hide();
-		$$('rPhoto').hide();
-		
-		vSel = $$('cSel').getValue();
-		
-		if (vSel === "Cours PDF") {
-			vUser = WAF.directory.currentUser().userName;
-			$$("LstPDF").setRowHeight(26);
-			sources.eleves.query("Utilisateur.Login = :1", { onSuccess: function(event) { 
-					var vEleID;
-					elem = sources.eleves;
-					if (elem.length > 0) {
-						vEleID = elem.ID;
-						sources.abonnes.query("Eleve.ID = :1", vEleID);
-						$$('LstPDF').show();
-						$$('btShow').show();
-						$$('rPhoto').show();
-						$$('sPDF').hide();
-					};		
-			}, params:[vUser] });
-		}
-		
-		
 	};// @lock
 
 	W_Login.login = function W_Login_login (event)// @startlock
@@ -75,9 +216,17 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("ListAssoc", "onRowDraw", ListAssoc.onRowDraw, "WAF");
+	WAF.addListener("btRight", "click", btRight.click, "WAF");
+	WAF.addListener("btLeft", "click", btLeft.click, "WAF");
+	WAF.addListener("btPlan", "click", btPlan.click, "WAF");
+	WAF.addListener("btNews", "click", btNews.click, "WAF");
+	WAF.addListener("btFilm", "click", btFilm.click, "WAF");
+	WAF.addListener("btPDF", "click", btPDF.click, "WAF");
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+	WAF.addListener("LstPDF", "onRowClick", LstPDF.onRowClick, "WAF");
 	WAF.addListener("btClose", "click", btClose.click, "WAF");
 	WAF.addListener("btShow", "click", btShow.click, "WAF");
-	WAF.addListener("cSel", "change", cSel.change, "WAF");
 	WAF.addListener("W_Login", "login", W_Login.login, "WAF");
 	WAF.addListener("W_Login", "logout", W_Login.logout, "WAF");
 // @endregion
